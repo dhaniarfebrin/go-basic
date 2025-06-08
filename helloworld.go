25548df5e368adcb719e4fb34f1ef5268af04218
dhaniarfebrin <dhaniarfebrinwahyudi@gmail.com>
2025-06-08 22:04:20 +0700

    Enhance main program with import statement and print output

--- helloworld.go
+++ helloworld.go
@@ -1,6 +1,9 @@
 package main
 
+import "fmt"
+
 func main() {
 	// This is a simple Go program that does nothing.
 	// It serves as a placeholder for future development.
+	fmt.Println("Hello, World!")
 }
