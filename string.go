39e111f7746abaa8ab2a06bcde62f09f6ddd6b87
dhaniarfebrin <dhaniarfebrinwahyudi@gmail.com>
2025-06-09 20:50:40 +0700

    Add string.go to demonstrate formatted output in Go

---
+++ string.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// This is a simple Go program that does nothing.
+	// It serves as a placeholder for future development.
+	fmt.Println("Dhani!")
+	fmt.Println("Dhaniar Febrin!")
+	fmt.Println("Dhaniar Febrin Wahyudi!")
+}
+
+// This program prints out a series of names.
+// It starts with "Dhani!", then adds "Dhaniar Febrin!", and finally "Dhaniar Febrin Wahyudi!".
+// The program demonstrates the use of the fmt package for formatted I/O in Go.
+// The fmt package provides functions for formatted I/O, such as Println for printing to the console.
