bf746dd44e4b4593b13823235bfa38f42bb0268d
dhaniarfebrin <dhaniarfebrinwahyudi@gmail.com>
2025-06-09 20:49:21 +0700

    Add boolean.go to demonstrate boolean values in Go

---
+++ boolean.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// This is a simple Go program that does nothing.
+	// It serves as a placeholder for future development.
+	fmt.Println("Benar = ", true)
+	fmt.Println("Salah = ", false)
+}
+
+// This program demonstrates the use of boolean values in Go.
+// It prints out the boolean values "true" and "false".
+// The boolean type is used to represent truth values in Go.
+// Boolean values are often used in conditional statements and logical operations.
